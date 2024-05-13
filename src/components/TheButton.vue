615fbda0331e6af4dc6f1d5635c0f1d63dc36119
Roman Kuzyk <roman.kuzyk.a@gmail.com>
2024-05-13 21:22:48 +0300

    add button component primary

---
+++ src/components/TheButton.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+export interface ButtonProps {
+    buttonVariant: 'primary' | 'secondary';
+}
+
+withDefaults(defineProps<ButtonProps>(), {
+    buttonVariant: 'primary',
+});
+</script>
+
+<template>
+    <button type="button" class="button" :class="`button-${buttonVariant}`">
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+.button {
+    font-weight: 700;
+    font-size: var(--font-size-regular);
+    line-height: 2rem;
+    letter-spacing: -0.011rem;
+    cursor: pointer;
+    transition: all 0.25s linear;
+}
+
+.button-primary {
+    padding: 0.8rem 1.4rem;
+    background-color: var(--color-secondary);
+    border: 0.125rem solid transparent;
+}
+
+.button-primary:hover {
+    background-color: var(--color-primary);
+    border-color: var(--color-secondary);
+    color: var(--color-secondary);
+}
+</style>
